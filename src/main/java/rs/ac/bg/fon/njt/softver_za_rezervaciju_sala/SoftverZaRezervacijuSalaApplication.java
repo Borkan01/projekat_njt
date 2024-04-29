c7cb9cd5583912cd84a99b86cae26476c4f9fbcc
stefan.segrt.5@gmail.com <Segi01>
2024-04-29 15:18:41 +0200

    Kreiran je projekat softver_za_realizaciju_sala

---
+++ src/main/java/rs/ac/bg/fon/njt/softver_za_rezervaciju_sala/SoftverZaRezervacijuSalaApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.njt.softver_za_rezervaciju_sala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftverZaRezervacijuSalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftverZaRezervacijuSalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/bg/fon/njt/softver_za_rezervaciju_sala/SoftverZaRezervacijuSalaApplicationTests.java
@@ -0,0 +1,11 @@
+package rs.ac.bg.fon.njt.softver_za_rezervaciju_sala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftverZaRezervacijuSalaApplicationTests {
+
+	
+
+}
