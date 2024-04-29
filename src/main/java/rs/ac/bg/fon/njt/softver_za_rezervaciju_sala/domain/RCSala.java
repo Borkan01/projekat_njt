f5cda50d24b16e0dd16b6f6b1fd005f52cdea2c2
stefan.segrt.5@gmail.com <Segi01>
2024-04-29 15:27:26 +0200

    SalaZaSastanke,RCSala,Ucionica,Kombinovana,Amfiteatar koje nasledjuju apstraktnu klasu Sala. Zatim domenske klase ZaposleniUNastavi i ZaposleniVanNastave koje nasledjuju apstraktnu klasu Zaposleni. Potom KorisnikZaposleni i Student koje nasledjuju apstraktnu klasu Korisnik. Nakon svega napravljena je asocijativna klasa Rezervacija od klasa Sala i Korisnik. Potom su dodati svi neophodni atributi i anotacije za svaku klasu.

---
+++ src/main/java/rs/ac/bg/fon/njt/softver_za_rezervaciju_sala/domain/RCSala.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rs.ac.bg.fon.njt.softver_za_rezervaciju_sala.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author Stefan
+ */
+@Entity
+@Table(name="rc_sala")
+@DiscriminatorValue("RC")
+public class RCSala extends Sala{
+    
+    @Column(name="broj_racunara")
+    private int brojRacunara;
+
+    public RCSala() {
+    }
+
+    public RCSala(int brojRacunara, String naziv, String napomena, int brojMesta,boolean status) {
+        super(naziv, napomena, brojMesta,status);
+        this.brojRacunara = brojRacunara;
+    }
+
+    @Override
+    public String toString() {
+        return "RCSala: "+super.toString()+" broj racunara: "+brojRacunara;
+    }
+    
+    
+
+}
